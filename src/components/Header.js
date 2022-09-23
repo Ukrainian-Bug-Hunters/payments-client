a6b98f653da5679f2bdd6236f588156d8572ae54
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-09-23 03:12:57 +0300

    add_header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Grommet, Header } from "grommet";
 
-const AppHeader = () => {
+const Header = () => {
     return (
-        <Grommet>
-            <Header background="brand" justify="center">
-                <h1 className="App-title">Payments</h1>
-            </Header>
-        </Grommet>
+        <header class="header-container">
+            <div class="header-sections">
+                <nav class="nav-container">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3790/3790699.png" alt="logo" className="app-logo" />
+                    <a href="../public/index.html" class="header-title">Pay<span>Ments</span></a>
+                </nav>
+                <ul class="navigation-contact-list">
+                    <li><a href="mailto:info@devstudio.com">info@payments.com</a></li>
+                    <li><a href="tel:+380961111111">+38 000 111 00 11</a></li>
+                </ul>
+            </div>
+        </header>
     )
 }
 
-export default AppHeader;
+export default Header;
