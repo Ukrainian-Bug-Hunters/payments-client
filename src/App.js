67566f35e8bff2ad7bab9c070218a67729593c64
Pavel <pavel.filkovskiy@gmail.com>
2022-09-24 09:31:36 +0100

    add header to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import { Grommet } from 'grommet';
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Grommet>
+      <Header />
       <Footer />
-    </>
+    </Grommet>
   )
 }
 
