f19c97e5948223c97c1b9f3e22ebb17ada4ed9f6
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-10-01 19:10:36 +0300

    added_payments_filter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, {useState} from "react";
 import {Select, TextInput, Button, Table, TableHeader, TableRow, TableCell, TableBody } from "grommet";
 import currencies from "../data/currencies";
@@ -107,3 +108,69 @@ const Main = () => {
 }
 
 export default Main;
+=======
+import React, {useState} from "react";
+import {Select, TextInput, Button } from "grommet";
+import currencies from "../data/currencies";
+// todo:
+// get all supported currencies from 
+// https://api.exchangerate.host/symbols
+import Payments from "./Payments";
+import './Main.css';
+
+
+const Main = () => {
+    const [foreingCurrency, setForeingCurrency] = useState('USD');
+    const [foreingAmount, setForeingAmount] = useState(0);
+    const [homeAmount, setHomeAmount] = useState(0);
+
+    function convert(){
+        fetch(`https://api.exchangerate.host/convert?from=${foreingCurrency}&to=GBP&amount=${foreingAmount}`)
+        .then(response => response.json())
+        .then(data => setHomeAmount(data.result));
+    }
+
+    function handleChangeForeingAmount(event){
+        if(Number(event.target.value) || event.target.value === ''){
+            setForeingAmount(event.target.value);
+        }
+        else{
+            event.target.value = foreingAmount;
+        }
+    }
+
+    return (
+        <main className="calculator-and-payments">
+            <section className="calc-section">
+                <h2 className="calc-title">Calculate payment in GBP</h2>
+                    <div className="calc-data-container">
+                            <Select className="convert-select"
+                                options={currencies}
+                                value={foreingCurrency}
+                                onChange={({currency}) => setForeingCurrency(currency)}
+                            />
+                            <TextInput className="calc-text-input"
+                                placeholder="type here"
+                                onChange={(event) => {
+                                    handleChangeForeingAmount(event);
+                                }}
+
+                            />
+                        <div className="calc-res">is worth</div>
+                            <TextInput className="calc-text-input" readOnly
+                                placeholder="type here"
+                                value={homeAmount}
+                            />
+                        <div>in GBP.</div>
+                    </div>
+                    <Button primary label="CALCULATE"
+                        onClick={convert}
+                    /> 
+            </section>
+            <Payments />
+        </main>
+    )
+}
+
+export default Main;
+>>>>>>> Stashed changes
---
+++ src/components/Payments.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Box, Tab, Tabs } from "grommet";
+import PaymentsTable from "./PaymentsTable";
+import payments from "../data/payments";
+
+const Payments = () => { 
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const tabNames = ["All Payments", "Completed", "Pending", "Cancelled"];
+
+    return (
+        <section className="payments-section">
+            <h2 className="payments-title">Payments</h2>
+            <Box align="center" pad="medium">
+                <Tabs activeIndex={activeTabIndex} onActive={setActiveTabIndex} justify="center">
+                    <Tab title={tabNames[0]}>
+                        <Box margin="small" gap="small">
+                            <PaymentsTable payments = {payments}/>
+                        </Box>
+                    </Tab>
+                    
+                    <Tab title={tabNames[1]}>
+                        <Box margin="small">8
+                            <PaymentsTable payments = {
+                                payments.filter(payment => payment.status === tabNames[1])
+                            }/>
+                        </Box>
+                    </Tab>
+
+                    <Tab title={tabNames[2]}>
+                        <Box margin="small">
+                            <PaymentsTable payments = {
+                                payments.filter(payment => payment.status === tabNames[2])
+                            }/>
+                        </Box>
+                    </Tab>
+
+                    <Tab title={tabNames[3]}>
+                        <Box margin="small">
+                            <PaymentsTable payments = {
+                                payments.filter(payment => payment.status === tabNames[3])
+                            }/>
+                        </Box>
+                    </Tab>
+                </Tabs>
+            </Box>
+        </section>
+    )
+}
+
+export default Payments;
